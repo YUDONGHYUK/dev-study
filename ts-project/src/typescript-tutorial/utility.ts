7737ddb7d8ffcc3dd96410a08000f38ab002d08b
doyu <ydh0905@nate.com>
2024-10-21 22:30:46 +0900

    feat: [typescript] type checker function

--- ts-project/src/typescript-tutorial/utility.ts
+++ ts-project/src/typescript-tutorial/utility.ts
@@ -14,3 +14,16 @@ type Equal<A, B> = A extends B ? (B extends A ? 1 : 0) : 0;
 
 type Equal1 = Equal<1, 0>; // 0
 type Equal2 = Equal<1, 1>; // 1
+
+const Pass = 1;
+const Fail = 0;
+
+/**
+ * 두 개의 제네릭 타입이 같은지 비교하는 함수
+ * @param params : 비교의 결과를 params로 받는다.
+ */
+declare function check<A, B>(params: Equal<Equal<A, B>, typeof Pass>): void;
+
+check<Head<[1, 2, 3, 4]>, 1>(Pass);
+check<Head<[]>, undefined>(Pass);
+check<Head<[4, 2, 3]>, 4>(Pass);
